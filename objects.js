a05aafb639abd7257cb9e14f98272b69da620117
Peter Kim <peterckim96@gmail.com>
2018-04-20 03:56:32 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -10,7 +10,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
 
 function removeFromPlaylist(playlist, artistName) {
   console.log(playlist);
-  delete playlist.artistName;
+  delete playlist[artistName];
   console.log(playlist);
   return playlist;
 }
